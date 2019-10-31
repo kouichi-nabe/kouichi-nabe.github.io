49d0e1a793f5e807c56c58a1e633d4e653772a16
kouichi@name <daiya.adgjmp1014@gmail.com>
2019-11-01 05:27:06 +0900

    fix files

---
+++ js/changeImage.js
@@ -0,0 +1,34 @@
+$(function(){
+  const headInner = $('.head__inner');
+
+  const images = [
+    'url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_01.jpeg)',
+    'url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_02.jpg)',
+    'url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_03.jpg)',
+    'url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_04.jpeg)',
+    'url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_05.jpg)'
+  ];
+
+  headInner.css({ backgroundImage: 'url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_03.jpg)' });
+
+  let num = 0;
+
+  var chagneImage = function(){
+    if(num <= 4){
+      let imageUrl = images[num]
+      headInner.css({ backgroundImage: imageUrl });
+      num = num + 1;
+    } else {
+      num = 0;
+    }
+  }
+
+  var ok = function(){
+    console.log("man")
+  }
+
+  setInterval(chagneImage, 5000);
+  setInterval(ok, 1000);
+
+  // headInner.css({ backgroundImage: "url(file:///Users/watanabe_kouichi/kouichi-nabe.github.io/images/head_02.jpg)" });
+})
